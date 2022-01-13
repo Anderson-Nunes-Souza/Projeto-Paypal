a5e6e0baf29a5c7cbee11cba730940c7cefff270
Anderson <anderson.nunes.souza@hotmail.com>
2022-01-13 02:24:58 -0300

    Integracao Serverside Criada
    - Criada chamada na function.php para gerar o Access Token.
    - Criada chamada na function.php para gerar o ordem ID.
    - Criado inputs no front end para campos do comprador (Country,Zipcode,ETC);
    
    ** Próximos ajustes: **
    - Criar retorno de Redirecionamento (OnApproval,OnError);
    - Ajustar inputs dos campos do comprador para redirecionar dados para a integração;
    - Criar a "thank you page".

--- index.php
+++ index.php
@@ -6,15 +6,19 @@
     <!--  This file has been downloaded from bootdey.com @bootdey on twitter -->
     <!--  All snippets are MIT license http://bootdey.com/license -->
     <title>shopping cart - Bootdey.com</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" href="//netdna.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css">
     <script src="https://code.jquery.com/jquery-1.10.2.min.js"></script>
     <link href="https://netdna.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://netdna.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
     <script src="./js/shopping_cart.js"></script>
+    <!-- header paypal sdk-->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <title> PayPal Checkout Integration | Server Demo </title>
+    <!-- Fim header paypal SDK-->
 </head>
 
 <body>
-    <link rel="stylesheet" type="text/css" href="//netdna.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css">
     <div class="container bootstrap snippets bootdey">
         <div class="col-md-9 col-sm-8 content">
             <div class="row">
@@ -91,12 +95,75 @@
                                             <td>$<span>72.00</span></td>
                                         </tr>
                                     </tbody>
+                                    <t>
                                 </table>
+                                <!-- Campos de Cliente-->
+                                <form>
+                                    <div class="mb-3">
+                                        <label for="text" class="form-label">First Name</label>
+                                        <input type="text" class="form-control" id="buyerFirstName" value="Anderson">
+                                    </div>
+                                    <div class="mb-3">
+                                        <label for="text" class="form-label">Last Name</label>
+                                        <input type="text" class="form-control" id="buyerLastName" value="Souza">
+                                    </div>
+                                    <div class="mb-3">
+                                        <label for="email" class="form-label">Email address</label>
+                                        <input type="email" class="form-control" id="buyerEmail" aria-describedby="emailHelp" value="andersonbuyer@paypal.com">
+                                    </div>
+                                    <div class="mb-3">
+                                        <label for="number" class="form-label">Phone Number</label>
+                                        <input type="tel" class="form-control" id="buyerPhone">
+                                    </div>
+                                    <div>
+                                        <label for="address" class="form-label">Address</label>
+                                        <input type="tel" class="form-control" id="buyerAddres">
+                                    </div>
+                                    <div>
+                                        <label for="Country" class="form-label">Country</label>
+                                        <input type="country" class="form-control" id="buyerCountry" aria-describedby="PhoneHelp" value="">
+                                    </div>
+                                    <div>
+                                        <label for="State" class="form-label">State</label>
+                                        <input type="" class="form-control" id="buyerState" aria-describedby="StateHelp" value="">
+                                    </div>
+                                    <div>
+                                        <label for="ZipCode" class="form-label">ZipCode</label>
+                                        <input type="number" class="form-control" id="buyerNumber" aria-describedby="" value="01203000">
+                                    </div>
+                                    <div>
+                                        <label for="Street" class="form-label">Street</label>
+                                        <input type="tel" class="form-control" id="buyerCountry" aria-describedby="PhoneHelp" value="">
+                                    </div>
+                                </form>
+                                <!--utilizando br para pular linha-->
+                                <br>
+                                <center>
+                                <!--Fim campos de cliente -->
+
+                                <!-- botão Paypal -->
+                                <!-- Set up a container element for the button -->
+                                <div id="paypal-button-container"></div>
+
+                                <!-- Include the PayPal JavaScript SDK -->
+                                <script src="https://www.paypal.com/sdk/js?client-id=AUGmAdOyjusVsx_rh4vhf0P-zaAE1S2HpFH9u9F8SMfQNS6EFDrG9C5mkL7gfgCyTbgeSf621VRgZRSe&currency=USD"></script>
+
+                                <script>
+                                    // Render the PayPal button into #paypal-button-container
+                                    paypal.Buttons({
+                                        createOrder: async () => {
+                                            const response = await fetch("./function.php")
+                                            const data = await response.text()
+                                            console.log(data)
+                                            return data
+                                        }
+                                    }).render('#paypal-button-container');
+                                </script>
+                                <!--fim codigo paypal-->
+                                </center>                        
                             </div>
                         </div>
                     </div>
-                    <a href="#" class="btn btn-success"><span class="glyphicon glyphicon-arrow-left"></span>&nbsp;Continue Shopping</a>
-                    <a href="#" class="btn btn-primary pull-right">Next<span class="glyphicon glyphicon-chevron-right"></span></a>
                 </div>
             </div>
         </div>
@@ -110,15 +177,15 @@
             margin-left: auto;
             margin-right: auto;
         }
-        
+
         table tr td {
             border: 1px solid #FFFFFF;
         }
-        
+
         table tr th {
             background: #eee;
         }
-        
+
         .panel-shadow {
             box-shadow: rgba(0, 0, 0, 0.3) 7px 7px 7px;
         }
