7938e758b599276742e4033b46ba6f2d9bfcd280
Anderson <anderson.nunes.souza@hotmail.com>
2022-01-14 02:15:42 -0300

    Thankyou page redirecionando para site hospedado

--- Thankyou.php
+++ Thankyou.php
@@ -19,7 +19,7 @@
             Having trouble? <a href="">Contact us</a>
         </p>
         <p class="lead">
-            <a class="btn btn-primary btn-sm" href="http://localhost/Projeto-Paypal/" role="button">Continue to homepage</a>
+            <a class="btn btn-primary btn-sm" href="http://projetopaypalanderson-online.preview-domain.com/" role="button">Continue to homepage</a>
         </p>
     </div>
 
